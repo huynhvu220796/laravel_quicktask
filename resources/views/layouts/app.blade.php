8f3536d9b0d396981614d0a424a9c0671e1ea87e
huynhvu220796 <huynhvu220796@gmail.com>
2018-01-18 15:57:13 +0700

    add quicktask_1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,8 +39,8 @@
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
+                            <li><a class="nav-link" href="{{ route('login') }}">Login</a></li>
+                            <li><a class="nav-link" href="{{ route('register') }}">Register</a></li>
                         @else
                             <li class="nav-item dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
